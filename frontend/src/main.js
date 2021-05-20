9484ca3f0456a66c57c9bac951b97dd6c1c2bd9d
Bee-ux <binetwoo@gmail.com>
2021-05-20 22:54:30 +0200

    Ajout de font awesome

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,31 @@
 import Vue from "vue/dist/vue.js"
 import App from './App.vue'
 import VueRouter from 'vue-router'
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faLinkedin,
+  faTwitter,
+  faFacebookMessenger,
+  faInstagram,
+  faMedium
+} from "@fortawesome/free-brands-svg-icons";
+
+
+library.add(
+  faCircleNotch,
+  faLinkedin,
+  faTwitter,
+  faFacebookMessenger,
+  faInstagram,
+  faMedium
+);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
 import { BootstrapVue } from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
@@ -16,6 +41,8 @@ import Contact from './page/Contact'
 import Works from './page/Works'
 import Talent from './page/Talent'
 import Agence from './page/Agence'
+import Mentions from './page/Mentions'
+
 
 
 const routes = [
@@ -23,6 +50,8 @@ const routes = [
   { path: '/works', name: 'works', component: Works },
   { path: '/talent', name: 'talent', component: Talent },
   { path: '/agence', name: 'agence', component: Agence },
+  { path: '/Mentions', name: 'Mentions', component: Mentions },
+
 ]
 
 const router = new VueRouter({
