e024e1f85e64a9f67a608ebbaacd17411afe02c5
Raphael Tomic <raphael.tomic@gmail.com>
2021-05-22 21:59:14 +0200

    Codage contenu

--- frontend/src/page/Works.vue
+++ frontend/src/page/Works.vue
@@ -1,8 +1,78 @@
 <template>
-  <div>
-    <h1>{ Works }</h1>
-  </div>
+  <b-container>
+    <h1 class="text-center">{ Works }</h1>
+    <b-row class="mb-5">
+      <b-col class="clients">
+        <a href="#">
+          <img src="../assets/image/logo-simple.jpg" alt="logo du client" />
+        </a>
+        <p>test work</p>
+      </b-col>
+      <b-col class="clients">
+        <a href="#">
+          <img src="../assets/image/logo-simple.jpg" alt="logo du client" />
+        </a>
+        <p>test work</p>
+      </b-col>
+      <b-col class="clients">
+        <a href="#">
+          <img src="../assets/image/logo-simple.jpg" alt="logo du client" />
+        </a>
+        <p>test work</p>
+      </b-col>
+    </b-row>
+    <b-row class="mb-5">
+      <b-col class="clients">
+        <a href="#">
+          <img src="../assets/image/logo-simple.jpg" alt="logo du client" />
+        </a>
+        <p>test work</p>
+      </b-col>
+      <b-col class="clients">
+        <a href="#">
+          <img src="../assets/image/logo-simple.jpg" alt="logo du client" />
+        </a>
+        <p>test work</p>
+      </b-col>
+      <b-col class="clients">
+        <a href="#">
+          <img src="../assets/image/logo-simple.jpg" alt="logo du client" />
+        </a>
+        <p>test work</p>
+      </b-col>
+    </b-row>
+    <b-row class="mb-5">
+      <b-col class="clients">
+        <a href="#">
+          <img src="../assets/image/logo-simple.jpg" alt="logo du client" />
+        </a>
+        <p>test work</p>
+      </b-col>
+      <b-col class="clients">
+        <a href="#">
+          <img src="../assets/image/logo-simple.jpg" alt="logo du client" />
+        </a>
+        <p>test work</p>
+      </b-col>
+      <b-col class="clients">
+        <a href="#">
+          <img src="../assets/image/logo-simple.jpg" alt="logo du client" />
+        </a>
+        <p>test work</p>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <style scoped>
+img {
+  width: 150px;
+}
+
+.clients img,
+p {
+  display: block;
+  margin: 0 auto;
+  text-align: center;
+}
 </style>
