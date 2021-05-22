08374e9b96a7a14bc92c0b6dbce044064a7738c0
Raphael Tomic <raphael.tomic@gmail.com>
2021-05-22 21:48:02 +0200

    Ajout lien mention légales

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -17,6 +17,7 @@
         <p>10 rue du Terrage, 75010, Paris</p>
       </b-col>
       <b-col>
+        <b-row> </b-row>
         <div class="reseaux text-right">
           <a href="https://twitter.com/OneTwoFree20" target="_blank">
             <font-awesome-icon :icon="['fab', 'twitter']" size="3x" />
@@ -35,6 +36,19 @@
             <font-awesome-icon icon="linkedin"></font-awesome-icon>
           </a>
         </div>
+        <router-link
+          to="/Mentions"
+          custom
+          v-slot="{ href, navigate, isActive }"
+        >
+          <a
+            :active="isActive"
+            :href="href"
+            @click="navigate"
+            class="d-block text-right mt-3"
+            >Mentions légales</a
+          >
+        </router-link>
       </b-col>
     </b-row>
   </div>
@@ -59,6 +73,7 @@ a,
 .coordonnées {
   margin-right: 1em;
   color: black;
+  text-decoration: none !important;
 }
 
 .coordonnées {
