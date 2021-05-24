c6e735a333fc726d6ad301bdfbd95e00272b55b4
Bee-ux <binetwoo@gmail.com>
2021-05-24 23:50:20 +0200

    création page

---
+++ frontend/src/page/Accueil.vue
@@ -0,0 +1,56 @@
+<template id="marker2">
+  <b-container fluid>
+    <h3>Notre histoire</h3>
+
+    <img
+      data-aos="fade-right"
+      src="../assets/image/bouteillebulles.png"
+      alt="bouteille bulles agence"
+    />
+    <img
+      src="../assets/image/Champmobilebulles.png"
+      alt="mobile bulles agence"
+    />
+    <img
+      src="../assets/image/Champtablettebulles.png"
+      alt="tablette bulles agence"
+    />
+    <img
+      src="../assets/image/champordibulles.png"
+      alt="ordinateur bulles agence"
+    />
+  </b-container>
+</template>
+
+<style scoped>
+.parallax {
+  /* The image used */
+  background-image: url("../assets/image/slideone1aerobic.png");
+  /* Set a specific height */
+  min-height: 300px;
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.parallax2 {
+  /* The image used */
+  background-image: url("../assets/image/Slideagence1.jpg");
+
+  /* Set a specific height */
+  min-height: 300px;
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.profil {
+  width: 150px;
+}
+.section {
+  background-color: #dcdcdc;
+}
+</style>
