2059b7b3c62a3eec7a1315efaaa5d7955cedf5e2
Bee-ux <binetwoo@gmail.com>
2021-05-25 18:02:52 +0200

    mise en place animation

--- frontend/src/page/Accueil.vue
+++ frontend/src/page/Accueil.vue
@@ -2,27 +2,31 @@
   <b-container fluid style="background-color: #308c99">
     <h3>Nous</h3>
 
-    <div>
+    <!-- <div>
       <img
-        class="bouteillevide"
+        class="bouteillevide slide"
         src="../assets/image/bouteillevide.png"
         alt="bouteille vide agence"
-      />
-    </div>
-
-    <div>
-      <img
-        class="boutbulles"
-        src="../assets/image/bouteillebulles.png"
-        alt="bouteille bulles agence"
         data-aos="fade-down"
         data-aos-easing="linear"
         data-aos-duration="1500"
       />
+    </div> -->
+
+    <div>
+      <img
+        class="boutbulles slide"
+        src="../assets/image/bouteillebulles.png"
+        alt="bouteille bulles agence"
+        data-aos="fade-left"
+        data-aos-anchor="#example-anchor"
+        data-aos-offset="500"
+        data-aos-duration="4000"
+      />
     </div>
     <div>
       <img
-        class="mobilebulles"
+        class="mobilebulles slide"
         src="../assets/image/Champmobilebulles.png"
         alt="mobile bulles agence"
         data-aos="fade-right"
@@ -32,7 +36,7 @@
     </div>
     <div>
       <img
-        class="tablettebulles"
+        class="tablettebulles slide"
         src="../assets/image/Champtablettebulles.png"
         alt="tablette bulles agence"
         data-aos="fade-up"
@@ -41,7 +45,7 @@
     </div>
     <div>
       <img
-        class="ordibulles"
+        class="ordibulles slide"
         src="../assets/image/champordibulles.png"
         alt="ordinateur bulles agence"
         data-aos="fade-down"
@@ -64,20 +68,25 @@ export default {
 </script>
 
 <style >
-.boutvide {
-  width: 300px;
+.bouteillevide {
+  /* width: 300px; */
+  height: 75px;
 }
 .boutbulles {
-  width: 350px;
+  /* width: 350px; */
+  height: 150px;
 }
 .mobilebulles {
-  width: 100px;
+  /* width: 100px; */
+  height: 150px;
 }
 .tablettebulles {
-  width: 200px;
+  /* width: 200px; */
+  height: 150px;
 }
 .ordibulles {
-  width: 300px;
+  /* width: 300px; */
+  height: 150px;
 }
 body,
 nav.navbar,
