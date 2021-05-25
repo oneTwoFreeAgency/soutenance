7fbb3fc1b34b449f51d201e961c6cab55f182cbe
Raphael Tomic <raphael.tomic@gmail.com>
2021-05-25 17:01:50 +0200

    Mise en place component Modal

--- frontend/src/page/Works.vue
+++ frontend/src/page/Works.vue
@@ -55,21 +55,49 @@
         <p>test work</p>
       </b-col>
       <b-col class="clients">
-        <a href="#">
-          <img src="../assets/image/logo-simple.jpg" alt="logo du client" />
-        </a>
+        <img src="../assets/image/logo-simple.jpg" alt="logo du client" />
+        <button type="button" class="btn" @click="showModal">
+          Plus d'info
+        </button>
+
+        <Modal v-show="isModalVisible" @close="closeModal">
+          <template v-slot:header>
+            <h1>Le titre de ma page</h1>
+          </template>
+          <template v-slot:body>
+            <p>Ici les infos de contact</p>
+          </template>
+          <template v-slot:footer>
+            <p>Ici les infos de contact</p>
+          </template>
+        </Modal>
+
         <p>test work</p>
       </b-col>
     </b-row>
-    <Modal></Modal>
   </b-container>
 </template>
 
 <script>
-import { Modal } from "../components/Modal.vue";
+import Modal from "../components/Modal";
 
 export default {
-  components: Modal,
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      isModalVisible: false,
+    };
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
+  },
 };
 </script>
 
@@ -78,7 +106,10 @@ export default {
 img {
   width: 150px;
 }
-
+.btn {
+  display: block;
+  margin: 0 auto;
+}
 .clients img,
 p {
   display: block;
