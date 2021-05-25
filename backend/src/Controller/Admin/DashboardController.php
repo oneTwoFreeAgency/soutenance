cfbcd785879431cd49c1a27af93be9c9469f6216
Philippe Mariou <philippe.mariou@colombbus.org>
2021-05-25 15:50:10 +0200

    mail attachement ok

--- backend/src/Controller/Admin/DashboardController.php
+++ backend/src/Controller/Admin/DashboardController.php
@@ -4,7 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Brief;
 use App\Entity\Devis;
+use App\Entity\Concours;
 use App\Entity\Contacts;
+use App\Entity\Candidats;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -31,8 +33,8 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Brief', 'fas fa-list', Brief::class);
-        yield MenuItem::linkToCrud('Candidats', 'fas fa-list', Contacts::class);
-        yield MenuItem::linkToCrud('Concours', 'fas fa-list', Contacts::class);
+        yield MenuItem::linkToCrud('Candidats', 'fas fa-list', Candidats::class);
+        yield MenuItem::linkToCrud('Concours', 'fas fa-list', Concours::class);
         yield MenuItem::linkToCrud('Contacts', 'fas fa-list', Contacts::class);
         yield MenuItem::linkToCrud('Devis', 'fas fa-list', Devis::class);
     }
