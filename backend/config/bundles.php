d4170b3833c8531d28e83261f00042fd6083eca6
Phil4525 <philippe.mariou@colombbus.org>
2021-06-23 17:26:27 +0200

    supression Webpack-Encore

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -15,5 +15,4 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Gregwar\CaptchaBundle\GregwarCaptchaBundle::class => ['all' => true],
-    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
